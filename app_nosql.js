eae1fb4a0399b52d4221c8c5c2c8f2bf3f1a5adc
Ton <prasit.17.06.1997@gmail.com>
2019-03-04 03:33:23 +0700

    v 0.63  add drop ui

--- app_nosql.js
+++ app_nosql.js
@@ -2,6 +2,7 @@ var mongojs = require('./db')
 var express = require('express')
 var bodyParser = require('body-parser')
 var ejs = require('ejs')
+var passwdDrop = 1234567890
 var port = 5555
 
 var db = mongojs.connect;
@@ -11,20 +12,8 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.json());
 
 app.get('/',(req, res)=>{
-  res.render('index')
-})
-
-app.get('/detail',(req, res)=>{
-  db.logger.find((err, docs)=>{
-    let ind = Object.keys(docs).length-(1)
-    if(ind+1 == 0){
-      res.render('noData')
-    }else{
-      res.render('detail',{DevEUI:docs[ind].Logger.DevEUI_uplink.DevEUI, DevAddr:docs[ind].Logger.DevEUI_uplink.DevAddr,
-        loggerTime:docs[ind].Logger.DevEUI_uplink.Time, serverTime:docs[ind].Timestamp,
-        payload_hex:docs[ind].Logger.DevEUI_uplink.payload_hex, LrrRSSI:parseInt(docs[ind].Logger.DevEUI_uplink.LrrRSSI)})
-    }
-  })
+  var bangkokTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Bangkok"});
+  res.render('index',{serverTime: new Date(bangkokTime)})
 })
 
 app.get('/get',(req, res)=>{
@@ -40,24 +29,40 @@ app.get('/countObj',(req, res)=>{
   })
 })
 
-app.get('/drop/:passwd',(req, res)=>{
-  if(req.params.passwd=='1234'){
-    db.logger.drop((err, docs)=>{
-      console.log('drop completed!')
-      res.send('correct password!'+'\n'+'drop completed!')
+app.get('/detail',(req, res)=>{
+  db.logger.find((err, docs)=>{
+    let ind = Object.keys(docs).length-(1)
+    if(ind+1 == 0){
+      res.render('noData')
+    }else{
+      res.render('detail',{DevEUI:docs[ind].Logger.DevEUI_uplink.DevEUI, DevAddr:docs[ind].Logger.DevEUI_uplink.DevAddr,
+        loggerTime:docs[ind].Logger.DevEUI_uplink.Time, serverTime:docs[ind].Timestamp,
+        payload_hex:docs[ind].Logger.DevEUI_uplink.payload_hex, LrrRSSI:parseInt(docs[ind].Logger.DevEUI_uplink.LrrRSSI)})
+    }
+  })
+})
+
+app.get('/drop',(req, res)=>{
+  res.sendFile(__dirname+'/'+'dropFrom.html')
+})
+app.get('/resDrop',(req,res)=>{
+  if(req.query.passwd==passwdDrop){
+    db.logger.drop((err,docs)=>{
+      res.send('Drop Success!')
     })
   }else{
-    res.send('Incorrect password!')
+    res.send('Drop Fail!')
   }
 })
 
 app.post('/postLogger',(req, res)=>{
-    var json = {'Logger':req.body,'Timestamp':new Date().toISOString()};
-    db.logger.insert(json,(err, docs)=>{
-      console.log(docs);
-      res.send(docs);
-    })
+  var bangkokTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Bangkok"});
+  var json = {'Logger':req.body,'Timestamp':new Date(bangkokTime)};
+  db.logger.insert(json,(err, docs)=>{
+    console.log(docs);
+    res.send(docs);
   })
+})
 
 app.listen(port,()=>{
     console.log('listen on port '+port)
